33bdfa409723e817ffc7d6689b1cbda607187ef8
Sura Al-jilawi <suraaljailawi@gmail.com>
2020-01-24 15:56:43 +0000

    Automatically backed up by Learn

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -6,7 +6,55 @@ class Filters extends React.Component {
       <div className="ui form">
         <h3>Animal type</h3>
         <div className="field">
-          <select name="type" id="type" onChange {() => this.props.onChangeType(this.value)}>
+          <select name="type" id="type" onChange={() => this.props.onChangeType(this.value)}>
+            <option value="all">All</option>
+            <option value="cat">Cats</option>
+            <option value="dog">Dogs</option>
+            <option value="micropig">Micropigs</option>
+          </select>
+        </div>
+
+        <div className="field">
+          <button className="ui secondary button" onClick={() => this.props.onFindPetsClick()}>Find pets</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Filtersimport React from 'react'
+
+class Filters extends React.Component {
+  render() {
+    return (
+      <div className="ui form">
+        <h3>Animal type</h3>
+        <div className="field">
+          <select name="type" id="type" onChange={() => this.props.onChangeType(this.value)}>
+            <option value="all">All</option>
+            <option value="cat">Cats</option>
+            <option value="dog">Dogs</option>
+            <option value="micropig">Micropigs</option>
+          </select>
+        </div>
+
+        <div className="field">
+          <button className="ui secondary button" onClick={() => this.props.onFindPetsClick()}>Find pets</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Filtersimport React from 'react'
+
+class Filters extends React.Component {
+  render() {
+    return (
+      <div className="ui form">
+        <h3>Animal type</h3>
+        <div className="field">
+          <select name="type" id="type" onChange={() => this.props.onChangeType(this.value)}>
             <option value="all">All</option>
             <option value="cat">Cats</option>
             <option value="dog">Dogs</option>
